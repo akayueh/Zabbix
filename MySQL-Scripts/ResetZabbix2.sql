c18ee4519ef2d1028e7dc195b16ba250226d762a
Steven Yu <akayueh@gmail.com>
2015-07-16 12:44:58 +1000

    Create ResetZabbix2.sql

---
+++ MySQL-Scripts/ResetZabbix2.sql
@@ -0,0 +1,38 @@
+
+use zabbix;
+
+SET FOREIGN_KEY_CHECKS = 0;
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE zabbix.triggers SET value = 0 where value = 1;
+
+UPDATE zabbix.events set value =0 where value = 1;
+
+
+truncate table zabbix.events; 
+optimize table zabbix.events;
+
+truncate table escalations; 
+optimize table escalations;
+
+truncate table alerts; 
+optimize table alerts; 
+
+truncate table housekeeper; 
+optimize table housekeeper;
+update zabbix.ids set nextid = 1 where tablename = 'housekeeper'; 
+
+truncate table acknowledges; 
+optimize table acknowledges;
+update zabbix.ids set nextid = 1 where tablename = 'acknowledges';
+
+truncate table auditlog; 
+optimize table auditlog;
+update zabbix.ids set nextid = 1 where tablename = 'auditlog';  
+
+truncate table auditlog_details; 
+optimize table auditlog_details; 
+update zabbix.ids set nextid = 1 where tablename = 'auditlog_details'; 
+
+SET SQL_SAFE_UPDATES = 1;
+SET FOREIGN_KEY_CHECKS = 1;
